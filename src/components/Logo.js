b0bcf6391ce888134024caa46c994e0f06c1661f
andrewfront <81253130+andrewfront@users.noreply.github.com>
2023-05-16 22:24:46 +0300

    Add files via upload

---
+++ src/components/Logo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+const Logo = () => {
+  return <Wrapper>
+    <span>Clothes/</span>Shop
+  </Wrapper>
+}
+const Wrapper = styled.h3 `
+margin-bottom: 0;
+color: var(--clr-grey-1);
+span {
+    color:var(--clr-primary-5);
+}
+`
+export default Logo
